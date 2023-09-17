bc2d9fd071aad23f8802d0e5ef4873b3fbb34191
Michael <mag.codev@gmail.com>
2023-09-16 23:04:49 -0500

    principio

---
+++ torii/src/assets/js/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDMsgQGZB8OVoxi6q0RLjNyv4CG17s7Rrw",
+    authDomain: "toriiweb-7e710.firebaseapp.com",
+    projectId: "toriiweb-7e710",
+    storageBucket: "toriiweb-7e710.appspot.com",
+    messagingSenderId: "695282890281",
+    appId: "1:695282890281:web:1d3c11d57b56a983600e38",
+    measurementId: "G-94N2V7JQ7X"
+  };
+
+export default firebaseConfig;
